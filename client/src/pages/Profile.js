872af0f4faf4aec08eac14ec5a5d40cf14930431
Jonathan Cushing <firex42069@gmail.com>
2022-07-13 19:41:01 -0400

    add front-end authentication

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,7 @@
-import { useParams } from "react-router-dom";
+import Auth from "../utils/auth.js";
+import { Navigate, useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
-import { QUERY_USER } from "../utils/queries";
+import { QUERY_USER, QUERY_ME } from "../utils/queries";
 import ThoughtList from "../components/ThoughtList";
 import FriendList from "../components/FriendList";
 import React from 'react';
@@ -8,14 +9,26 @@ import React from 'react';
 const Profile = () => {
   const { username: userParam } = useParams();
 
-  const { loading, data } = useQuery(QUERY_USER, {
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam }
   });
 
-  const user = data?.user || {};
+  const user = data?.me || data?.user || {};
+
+  if(Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Navigate to="/profile" />;
+  }
 
   if(loading) {
     return <div>Loading...</div>;
+  }
+
+  if(!user?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this page. Use the navigation links above to sign up or log in!
+      </h4>
+    );
   }
 
   return (
